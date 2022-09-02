e37cf050a3bbbcc10939b47106528ed93e107a37
Ekaterina Terekhova <>
2022-09-02 17:01:10 +0000

    Initial commit

---
+++ HW1.js
@@ -0,0 +1,8 @@
+ let firstName = 'John';
+let lastName = ' Adams<br>';
+document.write(firstName, lastName);
+
+//firstName = 'Anna';
+//lastName = ' Karenina';
+//document.write(firstName, lastName);
+//console.log(firstName,lastName);
